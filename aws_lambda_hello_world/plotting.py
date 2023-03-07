53cccfa9fbd8b68878edb3e0491e1d008e36945d
alans <alans@DESKTOP-ARVIO1M>
2023-03-06 22:30:32 -0500

    changed histogram to CDF

--- aws_lambda_hello_world/plotting.py
+++ aws_lambda_hello_world/plotting.py
@@ -1,3 +1,8 @@
+# cdf
+# mean
+# variance
+# tail latency
+
 import matplotlib.pyplot as plt
 import pickle
 from matplotlib.pyplot import figure
@@ -14,12 +19,13 @@ max_instances = max([len(i) for i in all_times.values()])
 plt.tight_layout()
 plt.figure(figsize=(20, 6), dpi=80)
 
+print(all_times)
 figure, axis = plt.subplots(1, len(all_times.keys()))
 
 for ind, i in zip(range(len(all_times.keys())), all_times.keys()):
-    axis[ind].hist(all_times[i], rwidth=0.9)
-    axis[ind].set_xticks(np.arange(0, max_time+0.2, 0.2))
-    axis[ind].set_yticks(np.arange(0, max_instances+1, 1))
+    axis[ind].hist(all_times[i], cumulative=True, label='CDF', histtype='step', color='b')
+    axis[ind].set_xticks(np.arange(0, max_time+0.05, 0.05))
+    axis[ind].set_yticks(np.arange(0, max_instances+1, 5))
     axis[ind].set_title(f"{i} min delay")
     axis[ind].set_ylabel('Instances')
     axis[ind].set_xlabel('Execution time (s)')
@@ -27,6 +33,6 @@ for ind, i in zip(range(len(all_times.keys())), all_times.keys()):
 
 fig = plt.gcf()
 fig.set_size_inches(18.5, 10.5)
-fig.suptitle("Execution times for pinging Hello World", fontsize=20)
+fig.suptitle("CDF for instances of pinging Hello World", fontsize=20)
 
-plt.savefig('foo.pdf', bbox_inches='tight')
+plt.savefig('latency_plots.pdf', bbox_inches='tight')
