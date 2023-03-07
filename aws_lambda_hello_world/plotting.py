44f357d1ddb51c4a9ca05ede0c6c2b9d9690d952
alans <alans@DESKTOP-ARVIO1M>
2023-03-06 23:09:56 -0500

    added text box displaying mean, median, and standard deviation

--- aws_lambda_hello_world/plotting.py
+++ aws_lambda_hello_world/plotting.py
@@ -1,3 +1,8 @@
+# cdf
+# mean
+# variance
+# tail latency
+import matplotlib.ticker as mtick
 import matplotlib.pyplot as plt
 import pickle
 from matplotlib.pyplot import figure
@@ -14,19 +19,32 @@ max_instances = max([len(i) for i in all_times.values()])
 plt.tight_layout()
 plt.figure(figsize=(20, 6), dpi=80)
 
+print(all_times)
 figure, axis = plt.subplots(1, len(all_times.keys()))
 
 for ind, i in zip(range(len(all_times.keys())), all_times.keys()):
-    axis[ind].hist(all_times[i], rwidth=0.9)
-    axis[ind].set_xticks(np.arange(0, max_time+0.2, 0.2))
-    axis[ind].set_yticks(np.arange(0, max_instances+1, 1))
-    axis[ind].set_title(f"{i} min delay")
-    axis[ind].set_ylabel('Instances')
-    axis[ind].set_xlabel('Execution time (s)')
+    times = np.array(all_times[i])
+    ax = axis[ind]
+    mean = times.mean()
+    median = np.median(times)
+    std = times.std()
+    textstr = '\n'.join((
+    r'$\mu=%.2f$' % (mean, ),
+    r'$\mathrm{median}=%.2f$' % (median, ),
+    r'$\sigma=%.2f$' % (std, )))
+    ax.hist(times, density=True, cumulative=True, label='CDF', histtype='step', color='b')
+    ax.set_xticks(np.arange(0, max_time+0.05, 0.05))
+    ax.set_title(f"{i} min delay")
+    ax.set_ylabel('Percentage of instances')
+    ax.set_xlabel('Execution time (s)')
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+        verticalalignment='top', bbox=props)
 
 
 fig = plt.gcf()
 fig.set_size_inches(18.5, 10.5)
-fig.suptitle("Execution times for pinging Hello World", fontsize=20)
+fig.suptitle("CDF for instances of pinging Hello World", fontsize=20)
 
-plt.savefig('foo.pdf', bbox_inches='tight')
+plt.savefig('latency_plots.pdf', bbox_inches='tight')
