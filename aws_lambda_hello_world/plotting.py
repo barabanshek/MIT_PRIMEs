be0d9db9c9cbc63a2557aa61095c01434f8236df
alans <alans@DESKTOP-ARVIO1M>
2023-03-06 22:48:50 -0500

    fixed CDF function to show percentages

--- aws_lambda_hello_world/plotting.py
+++ aws_lambda_hello_world/plotting.py
@@ -2,7 +2,7 @@
 # mean
 # variance
 # tail latency
-
+import matplotlib.ticker as mtick
 import matplotlib.pyplot as plt
 import pickle
 from matplotlib.pyplot import figure
@@ -23,12 +23,17 @@ print(all_times)
 figure, axis = plt.subplots(1, len(all_times.keys()))
 
 for ind, i in zip(range(len(all_times.keys())), all_times.keys()):
-    axis[ind].hist(all_times[i], cumulative=True, label='CDF', histtype='step', color='b')
-    axis[ind].set_xticks(np.arange(0, max_time+0.05, 0.05))
-    axis[ind].set_yticks(np.arange(0, max_instances+1, 5))
-    axis[ind].set_title(f"{i} min delay")
-    axis[ind].set_ylabel('Instances')
-    axis[ind].set_xlabel('Execution time (s)')
+    mean = sum(all_times[i]) / len(all_times[i])
+    median = all_times[i][len(all_times[i])//2]
+    ax = axis[ind]
+    ax.hist(all_times[i], density=True, cumulative=True, label='CDF', histtype='step', color='b')
+    ax.set_xticks(np.arange(0, max_time+0.05, 0.05))
+    # axis[ind].set_yticks(np.arange(0, max_instances+1, 5))
+    ax.set_title(f"{i} min delay")
+    ax.set_ylabel('Percentage of instances')
+    ax.set_xlabel('Execution time (s)')
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+    print(mean, median)
 
 
 fig = plt.gcf()
