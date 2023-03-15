39675cce1512ec01a2088242c8dae6b8d5fa14d9
evanning1 <31423439+evanning1@users.noreply.github.com>
2023-03-15 19:25:33 -0400

    Update helloworld.py

--- aws_lambda_hello_world/helloworld.py
+++ aws_lambda_hello_world/helloworld.py
@@ -7,17 +7,22 @@ interval = int(input())
 print("iterations:")
 iterations = int(input())
 delays = {}
+
+#define function to request
 def a(interval2, iterations2):
     temp = []
     for i in range(iterations2):
         start = time.perf_counter()
-        os.system("curl http://127.0.0.1:3000/hello")
+        os.system("curl https://9wfkyhs07f.execute-api.ap-northeast-1.amazonaws.com/Prod/hello/")
         end = time.perf_counter()
         elapsed = end-start
         temp.append(str(elapsed))
         time.sleep(interval2)
     delays.update({interval2:temp})
+
 a(interval, iterations)
+
+#write the file
 with open("recorded_data.csv", "w", newline = '') as file:
     writer = csv.writer(file)
     writer.writerow(["interval", "time"])
