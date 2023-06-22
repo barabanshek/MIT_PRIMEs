9dc42174a3da4ec21befba2e843b7df6e7863a38
root <root@DESKTOP-ARVIO1M>
2023-06-22 17:40:27 -0400

    - changed the running format to the same as data_plotting -- now requires a parameter for the benchmark - dumps the graph in a subfolder of graphs titled "drop_rates" - added title to graph and updated axis labels - now prints the median values along with graphing

--- scripts/drop_rate_plotting.py
+++ scripts/drop_rate_plotting.py
@@ -4,34 +4,45 @@ import pickle
 from matplotlib.pyplot import figure
 import numpy as np
 import seaborn as sns
+import argparse
 
-with open('drop_rates.pickle', 'rb') as handle:
-    drop_rates = pickle.load(handle)
-drop_rates = dict(sorted(drop_rates.items()))
-print(drop_rates)
-sns.set_style('darkgrid')
+def main(args):
+    benchmark = args.benchmark
+    with open(f'./data/{benchmark}/{benchmark}_drop_rates.pickle', 'rb') as handle:
+        drop_rates = pickle.load(handle)
+    drop_rates = dict(sorted(drop_rates.items()))
+    sns.set_style('darkgrid')
 
-xs = np.array(list(drop_rates.keys()))
-ys = np.array(list(drop_rates.values()))
+    xs = np.array(list(drop_rates.keys()))
+    ys = np.array(list(drop_rates.values()))
 
-medians = {}
-uppers = {}
-lowers = {}
-for i in range(len(xs)):
-    medians[xs[i]] = np.median(ys[i])
-    std = np.std(ys[i])
-    uppers[xs[i]] = std
-    lowers[xs[i]] = std
+    medians = {}
+    uppers = {}
+    lowers = {}
+    for i in range(len(xs)):
+        medians[xs[i]] = np.median(ys[i])
+        std = np.std(ys[i])
+        uppers[xs[i]] = std
+        lowers[xs[i]] = std
 
-print(lowers)
-print(uppers)
-median_xs = np.array(list(medians.keys()))
-median_ys = np.array(list(medians.values()))
-errors = np.array([[lowers[median_xs[j]], uppers[median_xs[j]]] for j in range(len(uppers))]).T
+    median_xs = np.array(list(medians.keys()))
+    median_ys = np.array(list(medians.values()))
+    val_dict = {}
+    for i in range(len(median_xs)):
+        val_dict[median_xs[i]] = median_ys[i]
+    print(f'ALL RPS DELTA MEDIAN VALUES: {val_dict}')
+    errors = np.array([[lowers[median_xs[j]], uppers[median_xs[j]]] for j in range(len(uppers))]).T
 
-plt.errorbar(median_xs, median_ys, yerr=errors)
+    plt.errorbar(median_xs, median_ys, yerr=errors)
+    plt.title(f"Target RPS vs. Request Drop Rate for {benchmark}")
+    plt.xlabel('Target RPS')
+    plt.ylabel('Request Drop Rate')
+    print('done')
+    plt.savefig(f'./graphs/drop_rates/{benchmark}_drop_rates.pdf')
 
-plt.xlabel('rps')
-plt.ylabel('drop rate')
-print('done')
-plt.savefig('drop_rates.pdf')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--benchmark')
+    args = parser.parse_args()
+
+    main(args)
