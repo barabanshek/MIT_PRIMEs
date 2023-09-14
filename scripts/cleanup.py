274c76baf5f20da1a7625d62f4341a55eb8b88dc
als2005 <als2005@localhost>
2023-09-13 20:59:23 -0600

    add additional yaml file deletion to cleanup

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -1,4 +1,17 @@
 from subprocess import run
+import os
+
+def delete_files_in_directory(directory_path):
+   try:
+     files = os.listdir(directory_path)
+     for file in files:
+       file_path = os.path.join(directory_path, file)
+       if os.path.isfile(file_path):
+         os.remove(file_path)
+     print("All files deleted successfully.")
+   except OSError:
+     print("Error occurred while deleting files.")
+
 
 def main():
     run('''kubectl delete deployment --all''', shell=True)
@@ -6,5 +19,6 @@ def main():
     run('''kubectl delete hpa --all''', shell=True)
     run('''kubectl apply -f ~/vSwarm/benchmarks/hotel-app/yamls/knative/database.yaml''', shell=True)
     run('''kubectl apply -f ~/vSwarm/benchmarks/hotel-app/yamls/knative/memcached.yaml''', shell=True)
+    delete_files_in_directory('./k8s-yamls/tmp/')
 if __name__ == '__main__':
     main()
