ff8a7e4a011594d8018867247c84f990707f2d14
als2005 <als2005@localhost>
2023-09-20 16:57:44 -0600

    - added function to compute percent difference in target and real rps - put cleaning data in its own function - improved plotting function - small bug fix in getting correlation table - create correlation table heatmap - added metric type as argument for correlation analysis - added columns for cpu/mem requests and limits, and for RPS difference - save data to separate folder

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -3,19 +3,13 @@ import argparse
 import pickle
 import os
 import matplotlib.pyplot as plt
-
+import numpy as np
 from scipy import stats
 
 from pprint import pprint
 
 def first_index(arr):
     return arr[0]
-
-def plot_data(xs, ys, xlabel, ylabel):
-    plt.scatter(xs, ys)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.savefig('plot.png')
 
 def standardize_format(entry):
     try:
@@ -27,7 +21,13 @@ def standardize_format(entry):
             return 'skip'
     except:
         return entry
-    
+
+def compute_rps_deltas(reals, targets):
+    deltas = []
+    for real, target in zip(reals, targets):
+        # Compute percent difference.
+        deltas.append(np.abs(real - target)/np.mean([real, target]))
+    return deltas
 class Data:
     def __init__(self, df, data_id):
         self.df = df
@@ -36,9 +36,28 @@ class Data:
         # Clean data with 0 CPU utilization.
         index = self.df[self.df['cpu_util'].map(standardize_format) == 0].index
         self.df.drop(index, inplace=True)
-        print("Cleaned.")
-        cleaned_data_file = f"cleaned_data_{self.data_id}.csv"
+        
+    def get_cleaned_data(self, folder='./data-analysis'):
+        cleaned_data_file = f"{folder}/cleaned_data_{self.data_id}.csv"
         self.df.to_csv(cleaned_data_file, columns=self.df.columns)
+        print("Cleaned.")
+
+    def plot_data(self, xlabel, ylabel, save_folder, title):
+        xs = []
+        ys = []
+        for m1, m2 in zip(self.df[xlabel], self.df[ylabel]):
+            if standardize_format(m1) != 'skip' and standardize_format(m2) != 'skip':
+                xs.append(standardize_format(m1))
+                ys.append(standardize_format(m2))
+
+        plt.scatter(xs, ys)
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.plot(np.unique(xs), np.poly1d(np.polyfit(xs, ys, 1))(np.unique(xs)), color='r')
+
+        plt.savefig(f'{save_folder}/{xlabel}-{ylabel}_plot.png')
+        print('Done plotting.')
 
     def get_correlations(self, col1, col2, metric='pearson'):
         xs = []
@@ -59,24 +78,25 @@ class Data:
         return round(stat.statistic, 5)        
         # print(f'Correlation using metric {metric}: {round(stat.statistic, 5)}')
 
-    def get_correlation_table(self, ignored_columns=[]):
+    def get_correlation_table(self, metric='pearson', ignored_columns=[]):
         for col1 in self.df.columns:
             if col1 not in ignored_columns:
                 self.table['column'] = [col2 for col2 in self.df.columns if col2 not in ignored_columns]
-                self.table[col1] = [self.get_correlations(col1, col2) for col2 in self.df.columns if col2 not in ignored_columns]
+                self.table[col1] = [self.get_correlations(col1, col2, metric=metric) for col2 in self.df.columns if col2 not in ignored_columns]
         table_df = pd.DataFrame(data=self.table)
         return table_df
 
 def main(args):
     data_file = args.f
+    metric = args.m
     data_id = data_file[10:20]
     with open(data_file, 'rb') as handle:
         data = pickle.load(handle)
         df = pd.DataFrame(data)
-    # with open(f'./data/successes_{data_id}.pickle', 'rb') as handle:
-    #     successes = pickle.load(handle)
-    #     print(f'Benchmarks: {len(successes)}')
-    #     print(f'Successes: {sum(successes)}')
+    with open(f'./data/successes_{data_id}.pickle', 'rb') as handle:
+        successes = pickle.load(handle)
+        print(f'Benchmarks: {len(successes)}')
+        print(f'Successes: {sum(successes)}')
     pd.set_option('display.max_columns', None)
     # remove RPS column
     df.columns = ['timestamp',
@@ -84,6 +104,7 @@ def main(args):
                   'cpu_util',
                   'mem_util',
                   'replicas',
+                  'cpu_requests', 'cpu_limits', 'mem_requests', 'mem_limits',
                   'duration', 
                   'issued', 
                   'completed', 
@@ -93,33 +114,33 @@ def main(args):
                   'avg_cpu_idle', 'avg_cpu_user', 'avg_cpu_system',
                               'avg_mem_free',
                               'avg_net_transmit (bps)', 'avg_net_receive (bps)']
-    df['rps_delta'] = df['rps_real'] - df['rps_target']
-    df.to_csv(f"data_{data_id}.csv", columns=df.columns)
+    df['rps_delta'] = compute_rps_deltas(df['rps_real'], df['rps_target'])
+    folder = './data-analysis'
+    df.to_csv(f"{folder}/data_{data_id}.csv", columns=df.columns)
     data_object = Data(df, data_id)
-    # col1 = 'rps_delta'
-    # col2 = '50th'
-    table_df = data_object.get_correlation_table(ignored_columns=['timestamp', 'benchmark'])
-    table_df.to_csv(f"correlation_table_{data_id}.csv", columns=table_df.columns)
+    data_object.get_cleaned_data(folder)
+
+    ignored_columns = ['timestamp', 
+                       'benchmark', 
+                       'cpu_requests', 'cpu_limits', 'mem_requests', 'mem_limits',
+                       'avg_cpu_idle', 
+                       'avg_cpu_user', 
+                       'avg_cpu_system',
+                        'avg_mem_free',
+                        'avg_net_transmit (bps)', 
+                        'avg_net_receive (bps)']
+    
+    table_df = data_object.get_correlation_table(metric=metric, ignored_columns=ignored_columns)
+    table_df.to_csv(f"{folder}/{metric}_correlation_table_{data_id}.csv", columns=table_df.columns)
     table_df.style.background_gradient(cmap ='viridis')\
             .set_properties(**{'font-size': '20px'})
     table_df.style.background_gradient(cmap ='viridis')\
-        .to_excel('./correlations.xlsx', engine='openpyxl')
+        .to_excel(f'{folder}/{metric}_correlations_{data_id}.xlsx', engine='openpyxl')
     print("Heatmap saved.")
-    # print(df)
-    # xs = [standardize_format(cpu) for cpu in df[col1]]
-    # ys = [standardize_format(rep) for rep in df[col2]]
-    # pearson = stats.pearsonr(xs, ys)
-    # spearman = stats.spearmanr(xs, ys)
-    # kendalltau = stats.kendalltau(xs, ys)
-    # print(pearson.statistic)
-    # print(spearman.statistic)
-    # print(kendalltau.statistic)
-    # plot_data(xs, ys, 'Average CPU %', 'Replicas')
-
-    # plot_data(xs, ys, col1, col2)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--f')
+    parser.add_argument('-m', default='pearson')
     args = parser.parse_args()
     main(args)
